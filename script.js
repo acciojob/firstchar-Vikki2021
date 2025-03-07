3df4e17f6ba28eef498f5e1208463d78efd6d878
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-08 00:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function firstChar(text) {
   // your code here
 	let str = text.trim();
 	return str.charAt(0);
+}
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
