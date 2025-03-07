184be0f68885a87f36e27e21351addd4f3bf6032
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-07 23:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	text = text.trim();
+	let arr = text.split("");
+	let ans = arr[0];
+	let res = ans.join("");
+	return res;
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
